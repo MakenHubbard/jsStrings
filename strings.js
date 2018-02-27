1304193785c1b312308796fad03e8a7ccde7946f
Maken Hubbard <makhub26@outlook.com>
2018-02-26 20:31:31 -0600

    excercise 2

--- strings.js
+++ strings.js
@@ -2,3 +2,9 @@
 
 var numOfSandwiches = 8;
 console.log("numOfSandwiches",numOfSandwiches);
+
+// Challenge #2
+
+var name = "Jeffery"
+var string = "Hey, there my boy " + name + ". how is it going?"
+console.log("string",string)
