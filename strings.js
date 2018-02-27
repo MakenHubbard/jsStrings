9bc2aedf0c6dc72ededc2bf1add8a62ec587ffc0
Maken Hubbard <makhub26@outlook.com>
2018-02-26 20:42:24 -0600

    adding excercise 3

--- strings.js
+++ strings.js
@@ -5,6 +5,12 @@ console.log("numOfSandwiches",numOfSandwiches);
 
 // Challenge #2
 
-var name = "Jeffery"
-var string = "Hey, there my boy " + name + ". how is it going?"
-console.log("string",string)
+var name = "Jeffery";
+var string = "Hey, there my boy " + name + ". how is it going?";
+console.log("string",string);
+
+// Challenge #3
+
+var DNA = "GCAT";
+var RNA = DNA.replace('T','U');
+console.log("RNA", RNA);
